c4f1849d8bbe639cb687acf45659adec14bee942
emertechie <emertechie@gmail.com>
2025-12-06 17:55:02 +0000

    Add Staff and Department, plus migration

--- SupportHub/Data/ApplicationDbContext.cs
+++ SupportHub/Data/ApplicationDbContext.cs
@@ -1,9 +1,20 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SupportHub.Models;
 
 namespace SupportHub.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
     : IdentityDbContext<ApplicationUser>(options)
 {
+    public DbSet<Staff> Staff { get; set; }
+    
+    public DbSet<Department> Departments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
 }
