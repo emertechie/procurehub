93823067ef739fe774cab09d57141c12b6e48e36
emertechie <emertechie@gmail.com>
2025-12-08 21:41:11 +0000

    fix: make Staff.DeparmentId nullable

--- SupportHub/Data/Migrations/20251208213854_FixStaffDeptId.Designer.cs
+++ SupportHub/Data/Migrations/20251208213854_FixStaffDeptId.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SupportHub.Data;
 
@@ -10,9 +11,11 @@ using SupportHub.Data;
 namespace SupportHub.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251208213854_FixStaffDeptId")]
+    partial class FixStaffDeptId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
