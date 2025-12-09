32565e73b9258f622f6be19bdc6044472c48b23c
emertechie <emertechie@gmail.com>
2025-12-09 14:10:05 +0000

    Refactor into methods

--- SupportHub.Web/Program.cs
+++ SupportHub.Web/Program.cs
@@ -7,62 +7,74 @@ using SupportHub.Web.Components.Account;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-builder.Services.AddCascadingAuthenticationState();
-builder.Services.AddScoped<IdentityRedirectManager>();
-builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
-
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultScheme = IdentityConstants.ApplicationScheme;
-        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-    })
-    .AddIdentityCookies();
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddIdentityCore<ApplicationUser>(options =>
-    {
-        options.SignIn.RequireConfirmedAccount = true;
-        options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddSignInManager()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+RegisterServices(builder);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
-
-app.UseAntiforgery();
-
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-// Add additional endpoints required by the Identity /Account Razor components.
-app.MapAdditionalIdentityEndpoints();
+ConfigureApplication(app);
 
 app.Run();
+
+return;
+
+static void RegisterServices(WebApplicationBuilder webApplicationBuilder)
+{
+    // Add services to the container.
+    webApplicationBuilder.Services.AddRazorComponents()
+        .AddInteractiveServerComponents();
+
+    webApplicationBuilder.Services.AddCascadingAuthenticationState();
+    webApplicationBuilder.Services.AddScoped<IdentityRedirectManager>();
+    webApplicationBuilder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+    webApplicationBuilder.Services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = IdentityConstants.ApplicationScheme;
+            options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+        })
+        .AddIdentityCookies();
+
+    var connectionString = webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection") ??
+                           throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+    webApplicationBuilder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlite(connectionString));
+    webApplicationBuilder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+    webApplicationBuilder.Services.AddIdentityCore<ApplicationUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = true;
+            options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
+        })
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddSignInManager()
+        .AddDefaultTokenProviders();
+
+    webApplicationBuilder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+}
+
+static void ConfigureApplication(WebApplication webApplication)
+{
+    // Configure the HTTP request pipeline.
+    if (webApplication.Environment.IsDevelopment())
+    {
+        webApplication.UseMigrationsEndPoint();
+    }
+    else
+    {
+        webApplication.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        webApplication.UseHsts();
+    }
+
+    webApplication.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+    webApplication.UseHttpsRedirection();
+
+    webApplication.UseAntiforgery();
+
+    webApplication.MapStaticAssets();
+    webApplication.MapRazorComponents<App>()
+        .AddInteractiveServerRenderMode();
+
+// Add additional endpoints required by the Identity /Account Razor components.
+    webApplication.MapAdditionalIdentityEndpoints();
+}
