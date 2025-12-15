5aa8532ee6ff4af40c30cd7ea6d9cf808625587d
emertechie <emertechie@gmail.com>
2025-12-15 17:57:46 +0000

    Remove pnpm due to bug breaking deploy
    https://github.com/dotnet/aspire/issues/13257

--- ProcureHub.AppHost/AppHost.cs
+++ ProcureHub.AppHost/AppHost.cs
@@ -3,7 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var api = builder.AddProject<Projects.ProcureHub_WebApi>("api");
 
 builder.AddViteApp("frontend", "../ProcureHub.WebApp")
-    .WithPnpm()
     .WithReference(api);
 
 builder.Build().Run();
