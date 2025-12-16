c87436369fa948ff3aefecd9a50d0313e7d0aeeb
emertechie <emertechie@gmail.com>
2025-12-16 17:56:13 +0000

    Add scripts to seed remote DB

---
+++ ProcureHub/Data/Scripts/1-AspNetRoles.sql
@@ -0,0 +1,3 @@
+INSERT INTO public."AspNetRoles" ("Id", "Name", "NormalizedName", "ConcurrencyStamp") VALUES ('d08b1bba-f41d-4313-bcf7-87fa97f57ea7', 'Admin', 'ADMIN', '301f6569-d121-43e6-86b1-bb6183de9d66');
+INSERT INTO public."AspNetRoles" ("Id", "Name", "NormalizedName", "ConcurrencyStamp") VALUES ('b17f73c6-10bc-4d20-a3bb-43ad45ecf324', 'Requester', 'REQUESTER', '62789f03-2c6f-45e9-8cb5-8c08e2daacab');
+INSERT INTO public."AspNetRoles" ("Id", "Name", "NormalizedName", "ConcurrencyStamp") VALUES ('98e5f982-51c3-41c2-a870-84d5c49d6753', 'Approver', 'APPROVER', '4a4b6e74-f167-4474-a05a-92a403131ead');
---
+++ ProcureHub/Data/Scripts/3-AspNetUserRoles.sql
@@ -0,0 +1,1 @@
+INSERT INTO public."AspNetUserRoles" ("UserId", "RoleId") VALUES ('12def9b8-42e0-41e0-af37-1903cc3db38f', 'd08b1bba-f41d-4313-bcf7-87fa97f57ea7');
