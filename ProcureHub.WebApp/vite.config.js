90e5a349c84491c1aa7d2f72aefafc5b8f54388f
emertechie <emertechie@gmail.com>
2025-12-17 15:27:27 +0000

    Initialise shadcn

--- ProcureHub.WebApp/vite.config.js
+++ ProcureHub.WebApp/vite.config.js
@@ -1,17 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({ target: 'react', autoCodeSplitting: true }),
     react(),
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     port: parseInt(process.env.VITE_PORT),
     proxy: {
-      // "api" is the name of the API in AppHost.cs.
+      // Proxy `/api` to `process.env.API_URL` to avoid local CORS issues
       '/api': {
         target: process.env.API_URL,
         changeOrigin: true,
