a94c94cd28b76225e95eb53c4505dba195ad343b
emertechie <emertechie@gmail.com>
2025-12-28 17:09:01 +0000

    Fix unique constraint check

--- ProcureHub/Common/DatabaseException.cs
+++ ProcureHub/Common/DatabaseException.cs
@@ -3,11 +3,12 @@ using Npgsql;
 
 namespace ProcureHub.Common;
 
-public static class DatabaseErrors
+public static class DatabaseException
 {
     public static bool IsUniqueConstraintViolation(DbUpdateException ex, string constraintName)
     {
         return ex.InnerException is PostgresException pgEx &&
-               pgEx.ConstraintName == constraintName;
+            pgEx.SqlState == "23505" &&
+            pgEx.ConstraintName == constraintName;
     }
 }
