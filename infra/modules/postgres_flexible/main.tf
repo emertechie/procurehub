357e439e5f6f9367192f4efb8236f9b440d87885
emertechie <emertechie@gmail.com>
2026-01-07 14:38:13 +0000

    Fix Postgres DB version in terraform

--- infra/modules/postgres_flexible/main.tf
+++ infra/modules/postgres_flexible/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_postgresql_flexible_server" "this" {
 
   sku_name   = var.sku_name
   storage_mb = var.storage_mb
-  version    = "16"
+  version    = "18"
 
   backup_retention_days        = var.backup_retention_days
   geo_redundant_backup_enabled = false
